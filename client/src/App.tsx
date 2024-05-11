7396a4281f3982db931de493f664ec858efad760
Bassem DIMASSI <bassem.dimassi@proxym-it.com>
2024-05-11 09:28:54 +0100

    :recycle: Refactoring code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,30 @@
 import {useState} from 'react';
 import './index.css';
 import axios from "axios";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-function App() {
+const axiosInstance = axios.create()
+
+axiosInstance.interceptors.response.use(
+    response => {
+        return Promise.resolve(response);
+    },
+    error => {
+        if (error.code === "ERR_NETWORK") {
+            window.location.href = '/login';
+        } else {
+            return Promise.reject(error);
+        }
+    }
+);
+
+function WhoAmI() {
 
     const [data, setData] = useState()
 
     const whoAmI = async () => {
         try {
-            const response = await axios.get('/api/me')
+            const response = await axiosInstance.get('/api/me', {withCredentials: true})
             setData(response.data)
         } catch (e) {
             setData("You are not logged in")
@@ -17,12 +33,31 @@ function App() {
 
     return (
         <div>
-            <form action="/api/auth" method="get">
-                <button type="submit">Login</button>
-            </form>
             <button onClick={whoAmI}>Who am I?</button>
             <p>{JSON.stringify(data)}</p>
         </div>
+    );
+}
+
+
+function LoginPage() {
+    return (
+        <div>
+            <form action="/api/auth" method="get">
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<WhoAmI/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+            </Routes>
+        </Router>
     );
 }
 
